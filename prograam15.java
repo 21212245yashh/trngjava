c6636fe0a9ed468722d132c2e1cad12a01faec81
21212245yashh <118956513+21212245yashh@users.noreply.github.com>
2023-03-27 10:33:20 +0530

    Add files via upload

---
+++ prograam15.java
@@ -0,0 +1,13 @@
+import java.util,Scanner;
+class program15
+{
+public static void main(String srgs[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number:");
+int num1=sc.next();//5
+//8*0
+//8*2=16
+//8*3=24
+}//main()
+}//program15
---
+++ program01.java
@@ -0,0 +1,7 @@
+class program01
+{
+public static void main (String args[])
+{
+System.out.println("hlo world");
+System.out.println("we are learning java");
+}}
